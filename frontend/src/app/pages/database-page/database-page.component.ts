8a25a07698876ab2ba44ad66d498558fb4f5508f
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2024-05-25 18:33:14 +0200

    resolved

--- frontend/src/app/pages/database-page/database-page.component.ts
+++ frontend/src/app/pages/database-page/database-page.component.ts
@@ -1,7 +1,10 @@
-import {Component, NgZone, ViewChild} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import {Router} from "@angular/router";
+import {io} from "socket.io-client";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-database-page',
@@ -11,6 +14,7 @@ import {HttpClient} from "@angular/common/http";
 export class DatabasePageComponent {
 
   @ViewChild('picker') picker: any;
+  socket: any;
 
   public color: ThemePalette = 'primary';
   ind: boolean = false;
@@ -18,22 +22,46 @@ export class DatabasePageComponent {
   isCalendarIconClicked: boolean = false;
   isCalendarOpen: boolean = false;
 
-
   public formGroup = new FormGroup({
-    date: new FormControl(null, [Validators.required]),
-    date2: new FormControl(null, [Validators.required])
-  })
+    date: new FormControl(null, [Validators.required])
+  });
 
-
-  ngOnInit() {
+  constructor(private router: Router){
+    this.socket = io('http://127.0.0.1:5000');
   }
 
+    closeClock() {
+    const alarmTime = this.formGroup.get('date')?.value;
+    console.log('Clock Deactivated:', alarmTime); // Debugging: Proverite vrednost
+    if (alarmTime) {
+      this.sendAlarmToBackend(alarmTime, false);
+    } else {
+      console.error('No date selected');
+    }
+  }
 
+  setClock() {
+    const alarmTime = this.formGroup.get('date')?.value;
+    console.log('Clock Activated:', alarmTime); // Debugging: Proverite vrednost
+    if (alarmTime) {
+      this.sendAlarmToBackend(alarmTime, true);
+    } else {
+      console.error('No date selected');
+    }
+  }
+
+sendAlarmToBackend(alarmTime: Date, isOn: boolean) {
+  const localAlarmTime = moment(alarmTime).format('YYYY-MM-DDTHH:mm:ss');
+  const alarmData = { alarmTime: localAlarmTime, isOn: isOn };
+  this.socket.emit('Clock', alarmData);
+  alert("Clock set: " + localAlarmTime);
+}
+
+  ngOnInit() {}
 
   closePicker() {
     this.picker.cancel();
   }
-
 
   hideButton() {
     this.ind = !this.ind;
